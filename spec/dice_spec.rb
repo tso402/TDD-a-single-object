3ce3fd9e20be6865a084cdfa064c2b14997d7f97
James Ault <james.ault@johnlewis.co.uk>
2019-02-08 11:25:30 +0000

    Test for randomness of roll passing

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -7,4 +7,15 @@ describe Dice do
   it 'returns a number between 1 and 6 when the player rolls the dice' do
     expect(subject.roll).to be_between(1,6)
   end
+
+  it 'returns a random number between 1 and 6 when the player rolls the dice' do
+    resultsA = []
+    resultsB = []
+    50.times {resultsA << subject.roll}
+    50.times {resultsB << subject.roll}
+    expect(resultsA).not_to equal(resultsB)
+  end
+
+
+
 end
