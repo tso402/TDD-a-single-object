d51951dc4ca3bdf72fa65485f36b59977da226f8
James Ault <james.ault@johnlewis.co.uk>
2019-02-08 12:22:34 +0000

    Return the total of a roll storey passing

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,12 +1,24 @@
 class Dice
 
+  def initialize
+    @rolls = []
+  end
+
   def roll
     return (rand(5) + 1)
   end
 
   def roller(num)
-    output = []
-    num.times {output << Dice.new.roll}
-    output
+    @rolls = []
+    num.times {@rolls << Dice.new.roll}
+    @rolls
+  end
+
+  def total
+    n = 0
+    @rolls.each do |roll|
+      n += roll
+    end
+    n
   end
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -1,6 +1,6 @@
 require './lib/dice.rb'
 describe Dice do
-  it 'a place can roll the dice' do
+  it 'a player can roll the dice' do
     expect(subject).to respond_to(:roll)
   end
 
@@ -20,8 +20,9 @@ describe Dice do
     expect(subject.roller(3).count).to eq 3
   end
 
-  
-
-
-
+  it 'shows the total score of a set of rolls when asked' do
+    dice = Dice.new
+    dice.roller(3)
+    expect(dice.total).to be_between(3,18)
+  end
 end
