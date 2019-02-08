640ba407d958d21a9f63dbbc5bc2a0f659282c5d
James Ault <james.ault@johnlewis.co.uk>
2019-02-08 10:52:17 +0000

    Dice returns a number between 1 and 6 user story passing

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,5 +1,6 @@
 class Dice
 
   def roll
+    return (rand(5) + 1)
   end
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -3,4 +3,8 @@ describe Dice do
   it 'a place can roll the dice' do
     expect(subject).to respond_to(:roll)
   end
+
+  it 'returns a number between 1 and 6 when the player rolls the dice' do
+    expect(subject.roll).to be_between(1,6)
+  end
 end
